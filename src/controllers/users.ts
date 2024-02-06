7a1b6fd8aeca20832035d51029a4b393ece04360
naufalzp <naufalarya0@gmail.com>
2024-02-06 09:05:45 +0700

    Add deleteUser functionality and isOwner middleware

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,12 +1,29 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
 
         return res.status(200).json(users).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.sendStatus(400);
+        }
+
+        const deletedUser = await deleteUserById(id);
+
+        return res.json(deletedUser).end();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,30 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../db/users";
 
+export const isOwner = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, "identity._id") as string;
+
+        if (!id || !currentUserId) {
+            return res.sendStatus(400);
+        }
+
+        if (id !== currentUserId.toString()) {
+            return res.sendStatus(403);
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
 export const isAuthenticated = async (
     req: express.Request,
     res: express.Response,
